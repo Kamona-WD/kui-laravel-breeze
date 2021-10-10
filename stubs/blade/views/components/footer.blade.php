188cbf61408ce3429ddf13201c60ad4427031a79
Kamona-WD <ak3849909@gmail.com>
2021-10-10 03:01:50 +0200

    fix footer style

--- stubs/blade/views/components/footer.blade.php
+++ stubs/blade/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="flex-shrink-0 px-6 py-4">
-    <p class="flex items-center justify-center gap-1 text-sm">
+    <p class="flex items-center justify-center gap-1 text-sm text-gray-600 dark:text-gray-400">
         <span>Made with</span>
         <span>
             <x-heroicon-s-heart class="w-6 h-6 text-red-500" />
