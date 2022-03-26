8a776f5d0a3c0c6c3858795b4abb74049d230635
Ahmed Kamel <ak3849909@gmail.com>
2022-03-26 14:42:54 +0200

    small fix

--- stubs/blade/views/components/input.blade.php
+++ stubs/blade/views/components/input.blade.php
@@ -5,7 +5,7 @@ $withiconClasses = $withicon ? 'pl-11 pr-4' : 'px-4'
 @endphp
 
 <input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
-'class' => $withiconClasses . 'py-2 border-gray-400 rounded-md focus:border-gray-400 focus:ring
+'class' => $withiconClasses . ' py-2 border-gray-400 rounded-md focus:border-gray-400 focus:ring
 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-white dark:border-gray-600 dark:bg-dark-eval-1
 dark:text-gray-300 dark:focus:ring-offset-dark-eval-1',
 ])
