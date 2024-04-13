66b004e9e5cad3f2ff19d8372b851038b0218f41
Ahsan Alamgir <ahsanalamgir14@gmail.com>
2024-04-14 03:25:47 +0500

    Fix path issue with vue inertia ssr for laravel ziggy package

--- stubs/vue/js/sfc/app.vite.js
+++ stubs/vue/js/sfc/app.vite.js
@@ -4,7 +4,7 @@ import '../css/app.css'
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m'
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/index.js'
 
 const appName =
     window.document.getElementsByTagName('title')[0]?.innerText || 'K UI'
